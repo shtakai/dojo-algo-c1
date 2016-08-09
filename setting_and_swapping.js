288ed2d8de8778a81db80c624bf03ca4dce7f44f
shtakai <shtakai@gmail.com>
2016-08-09 18:17:16 +0900

    setting and swapping

---
+++ setting_and_swapping.js
@@ -0,0 +1,10 @@
+let myNumber = 42
+let myName = 'Ichiroe'
+
+console.log('num', myNumber, 'name', myName)
+
+let tmp = myNumber
+myNumber = myName
+myName = tmp
+
+console.log('num', myNumber, 'name', myName)
