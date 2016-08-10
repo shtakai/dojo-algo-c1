f7c679cbd563a2540f4efa6ab4f57c1dac2c6f30
shtakai <shtakai@gmail.com>
2016-08-10 23:06:39 +0900

    fahrenheit_to_celcius

---
+++ fahrenheit_to_celcius.js
@@ -0,0 +1,9 @@
+function fahrenheitToCelcius(fDegrees){
+    return (fDegrees-32.0) * (5.0/9.0)
+}
+
+console.log(fahrenheitToCelcius(100));
+console.log(fahrenheitToCelcius(90));
+console.log(fahrenheitToCelcius(80));
+console.log(fahrenheitToCelcius(70));
+console.log(fahrenheitToCelcius(60));
